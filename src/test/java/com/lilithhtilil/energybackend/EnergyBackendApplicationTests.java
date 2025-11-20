62a646995c85afb3b392f49e085dab25a3475016
lilithhtilil <swiecaweronika6@gmail.com>
2025-11-20 13:22:10 +0100

    Initial commit

---
+++ src/test/java/com/lilithhtilil/energybackend/EnergyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilithhtilil.energybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
